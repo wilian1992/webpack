3f06298546917163c92e07114dffb75eeca17aee
oiuuoiuiououiouiououuoi <wilianpaiva11@gmail.com>
2022-02-28 21:18:52 -0300

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 function titleComponent(){
     const elemH1 = document.createElement('h1')
-    elemH1.innerHTML = 'hello world'
+    elemH1.innerHTML = 'hello world 2'
     return elemH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,37 @@
 const path = require('path')
-
+const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
 module.exports = {
 
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+module: {
+    rules: [
+     { 
+         test: /\.(sa|c|sc|)ss$/, // Expressão regular - regex
+        use: [
+            MiniCssExtract.loader,
+            'css-loader',
+            'sass-loader'
+
+         ]
+         
+      }
+
+    ]
+},
+  plugins: [
+       new HtmlWebpack({
+           filename: 'index.html',
+           template: './src/index.html'
+       }),
+       new MiniCssExtract({
+           filename: 'style.css'
+       })
+    ]   
 
 }
